724ceaef0e2ebe60445942a75a8d15bca5f1cdd7
Kavinayasri <146553129+Kavi-654@users.noreply.github.com>
2026-04-30 12:29:01 +0530

    Create MedicineResponseDTO.java

---
+++ src/main/java/com/mediremind/demo/dto/MedicineResponseDTO.java
@@ -0,0 +1,83 @@
+package com.mediremind.demo.dto;
+
+import com.mediremind.demo.model.Frequency;
+
+import java.time.LocalDate;
+
+public class MedicineResponseDTO {
+
+    private Long id;
+    private String name;
+    private String dosage;
+    private Frequency frequency;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private Long patientId;
+    private String patientName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDosage() {
+        return dosage;
+    }
+
+    public void setDosage(String dosage) {
+        this.dosage = dosage;
+    }
+
+    public Frequency getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(Frequency frequency) {
+        this.frequency = frequency;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public Long getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Long patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+
+}
