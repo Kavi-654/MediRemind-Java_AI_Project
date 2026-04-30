2490b24e4f498baf40f96c9a3125502868ebc78b
Kavinayasri <146553129+Kavi-654@users.noreply.github.com>
2026-04-30 12:20:40 +0530

    Create PatientRepository.java

---
+++ src/main/java/com/mediremind/demo/repository/PatientRepository.java
@@ -0,0 +1,17 @@
+package com.mediremind.demo.repository;
+
+import com.mediremind.demo.model.Patient;
+import jdk.jfr.Registered;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient,Long> {
+
+    Optional<Patient> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
+
